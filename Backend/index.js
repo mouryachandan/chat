8195bc3fce5d55ac88a79fe77b9b40c7ab850505
Chandan <ck43663@gmail.com>
2025-03-15 18:44:52 +0530

    done

--- Backend/index.js
+++ Backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cors from "cors";
+import path from "path";
 import cookieParser from "cookie-parser";
 
 import userRoute from "./routes/user.route.js";
@@ -28,6 +29,14 @@ try {
 //routes
 app.use("/api/user", userRoute);
 app.use("/api/message", messageRoute);
+if(process.env.NODE_ENV === "production"){
+  const dirPath = path.resolve();
+  app.use(express.static("./Frontend/dist"))
+  app.get("*",(req,res)=>{
+    res.sendFile(path.resolve(dirPath, "./Frontend/dist", "index.html"));
+
+  })
+}
 
 server.listen(PORT, () => {
   console.log(`Server is Running on port ${PORT}`);
